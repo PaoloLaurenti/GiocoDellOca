b2663532bcb7bd11248d775e95f0208250ed6284
Paolo Laurenti <laurentipaolo@gmail.com>
2015-03-30 14:09:11 +0200

    Game spec refactoring

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,6 +15,15 @@ describe AddPlayerUseCase, '"Gioco dell\'oca"' do
     (0...50).map { ('a'..'z').to_a[rand(26)] }.join
   end
 
+  def add_game_player(player_name)
+    AddPlayerUseCase.new(@game_id,
+                         player_name,
+                         @game_persistence_gateway,
+                         method(:update_players_names),
+                         method(:update_failure_message))
+                    .execute
+  end
+
   before :each do
     @presentable_players = []
     @initial_fail_message = get_random_text()
@@ -28,12 +37,7 @@ describe AddPlayerUseCase, '"Gioco dell\'oca"' do
     it 'allows the user to add the first player' do
       @game_persistence_gateway.setup_already_existing_game_events(@game_id, @game_events)
 
-      AddPlayerUseCase.new(@game_id,
-                           'Pippo',
-                           @game_persistence_gateway,
-                           method(:update_players_names),
-                           method(:update_failure_message))
-                      .execute
+      add_game_player('Pippo')
 
       expect(@presentable_players).to eq ['Pippo']
       expect(@fail_message).to eq @initial_fail_message
@@ -42,15 +46,10 @@ describe AddPlayerUseCase, '"Gioco dell\'oca"' do
 
   context 'when there is already a player' do
     it 'allows the user to add a second player' do
-      @game_events.events << 'PLAYER_ADDED@_@Pippo'
+      @game_events.events << 'PLAYER_ADDED@_@Pluto'
       @game_persistence_gateway.setup_already_existing_game_events(@game_id, @game_events)
 
-      AddPlayerUseCase.new(@game_id,
-                           'Pluto',
-                           @game_persistence_gateway,
-                           method(:update_players_names),
-                           method(:update_failure_message))
-                      .execute
+      add_game_player('Pippo')
 
       expect(@presentable_players).to match_array ['Pippo', 'Pluto']
       expect(@fail_message).to eq @initial_fail_message
