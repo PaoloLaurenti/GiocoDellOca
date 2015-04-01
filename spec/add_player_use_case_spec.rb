302e1ccedd835f786143fbaeb22fea5e61cba6fc
Paolo Laurenti <laurentipaolo@gmail.com>
2015-04-01 13:22:38 +0200

    Spec for adding player rename

---
+++ spec/add_player_use_case_spec.rb
@@ -0,0 +1,70 @@
+require 'use_case/add_player_use_case'
+require 'game_persistence_gateway_test_double'
+require 'game_events'
+
+describe AddPlayerUseCase, '"Gioco dell\'oca"' do
+  def update_players_names(players)
+    @presentable_players += players
+  end
+
+  def update_failure_message(message)
+    @fail_message = message
+  end
+
+  def get_random_text()
+    (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+  end
+
+  def add_game_player(player_name)
+    AddPlayerUseCase.new(@game_id, player_name, @game_persistence_gateway, method(:update_players_names),
+                         method(:update_failure_message))
+                    .execute
+  end
+
+  def verify_no_failure_message_has_been_notified
+    expect(@fail_message).to eq @initial_fail_message
+  end
+
+  before :each do
+    @presentable_players = []
+    @initial_fail_message = get_random_text()
+    @fail_message = @initial_fail_message
+    @game_id = SecureRandom.uuid
+    @game_persistence_gateway = GamePersistenceGatewayTestDouble.new
+    @game_events = GameEvents.new
+  end
+
+  context 'when there are no player' do
+    it 'allows the user to add the first player' do
+      @game_persistence_gateway.setup_already_existing_game_events(@game_id, @game_events)
+
+      add_game_player('Pippo')
+
+      expect(@presentable_players).to eq ['Pippo']
+      verify_no_failure_message_has_been_notified()
+    end
+  end
+
+  context 'when there is already a player' do
+    before :each do
+      @game_events.events << 'PLAYER_ADDED@_@Pippo'
+      @game_persistence_gateway.setup_already_existing_game_events(@game_id, @game_events)
+    end
+
+    it 'allows the user to add a second player' do
+      add_game_player('Pluto')
+
+      expect(@presentable_players).to match_array ['Pippo', 'Pluto']
+      verify_no_failure_message_has_been_notified()
+    end
+
+    context 'when the user tries to add a player with the same name of the first one' do
+      it 'notifies that the player already exists' do
+        add_game_player('Pippo')
+
+        expect(@presentable_players).to eq []
+        expect(@fail_message).to eq "Player 'Pippo' already exists"
+      end
+    end
+  end
+end
