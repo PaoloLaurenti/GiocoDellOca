1ca81c00bc98743af3f46122e5615a03b2f27877
Paolo Laurenti <laurentipaolo@gmail.com>
2015-04-01 13:43:35 +0200

    Move single player spec start

---
+++ spec/move_player_use_case_spec.rb
@@ -0,0 +1,16 @@
+require 'use_case/move_player_use_case'
+require 'game_events'
+
+describe MovePlayerUseCase do
+  context 'when there are only one player' do
+    it 'allows the user to move the player' do
+      # game_persistence_gateway = GamePersistenceGatewayTestDouble.new
+      #
+      # game_events = GameEvents.new
+      # game_events.events << 'PLAYER_ADDED@_@Pippo'
+      #
+      # use_case = MovePlayerUseCase.new(game_persistence_gateway)
+
+    end
+  end
+end
